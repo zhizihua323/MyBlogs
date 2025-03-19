cfa0c1ca6cc2b9355fc5546c2c9a76cafac1f7c7
zhizihua <2876290193@qq.com>
2025-03-19 13:09:03 +0800

    提交代码

---
+++ codes/j6-2-1/src/test/java/com/youkeda/YkdTest.java
@@ -0,0 +1,30 @@
+package com.youkeda;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.youkeda.comment.control.CommentController;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class YkdTest {
+
+    @LocalServerPort
+    int randomServerPort;
+    @Autowired
+    private CommentController commentController;
+    @Autowired
+    private ObjectMapper mapper;
+
+    public static void error(String msg) {
+        System.err.println("<YkdError>" + msg + "</YkdError>");
+    }
+
+    @Test
+    void contextLoads() throws Exception {
+
+
+    }
+
+}
